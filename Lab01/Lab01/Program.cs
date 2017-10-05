4a56a25a7187fb6a8c01945f6b04d208dbf770cb
benjaporn01 <59030179@kmitl.ac.th>
2017-10-05 22:28:48 +0700

    unfinish

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -6,21 +6,21 @@ namespace ConsoleApp5
 
     class Program
     {
-
-        static void Main(string[] args)
-        {
-            Person p = new Person();
-            p.SayHi();
-            Student s = new Student();
-            s.SayHi();
-            Person sp = new Student();
-            sp.SayHi();
-            Teacher t = new Teacher();
-            t.SayHi();
-            Person tp = new Teacher();
-            tp.SayHi();
-            Console.ReadKey();
-        }
+       
+static void Main(string[] args)
+{
+    Person p = new Person();
+    p.SayHi();
+    Student s = new Student();
+    s.SayHi();
+    Person sp = new Student();
+    sp.SayHi();
+    Teacher t = new Teacher();
+    t.SayHi();
+    Person tp = new Teacher();
+    tp.SayHi();
+    Console.ReadKey();
+}
 
     }
     class Vector
